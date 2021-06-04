a3ec3e6ef55c958993b3cc5db4ffd74ac52a6152
Pete Ansell <petea@microsoft.com>
2021-06-04 15:19:40 -0700

    Ran code clean-up.

--- Microsoft.Research.SpeechWriter/Microsoft.Research.SpeechWriter.Core/DefaultWriterEnvironment.cs
+++ Microsoft.Research.SpeechWriter/Microsoft.Research.SpeechWriter.Core/DefaultWriterEnvironment.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.Research.SpeechWriter.Core.Data;
-using Microsoft.Research.SpeechWriter.Core.Properties;
+﻿using Microsoft.Research.SpeechWriter.Core.Properties;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Research.SpeechWriter.Core
