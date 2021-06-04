7bd963bba7e6f9cb8e2b5a2d0ef8d8d92cd69ef4
Pete Ansell <petea@microsoft.com>
2021-06-04 14:11:26 -0700

    Added display of zero-length selections.

--- Microsoft.Research.SpeechWriter/EditBoxPrototype/MainPage.xaml.cs
+++ Microsoft.Research.SpeechWriter/EditBoxPrototype/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Research.SpeechWriter.Core.Data;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -33,8 +34,6 @@ namespace EditBoxPrototype
             var box = (TextBox)sender;
             var text = box.Text;
 
-            Block.Text = text;
-
             var sequence = TileSequence.FromRaw(text);
 
             ItemsContainer.Items.Clear();
@@ -42,6 +41,84 @@ namespace EditBoxPrototype
             {
                 ItemsContainer.Items.Add(child);
             }
+
+            var start = box.SelectionStart;
+            var length = box.SelectionLength;
+
+            Debug.WriteLine($"Selection is {start} for {length}");
+
+            var list = new List<TileData>();
+
+            var charPosition = 0;
+            var tilePosition = 0;
+            while (tilePosition < sequence.Count &&
+                charPosition + sequence[tilePosition].Content.Length <= start)
+            {
+                list.Add(sequence[tilePosition]);
+
+                charPosition += sequence[tilePosition].Content.Length;
+                tilePosition++;
+                if (tilePosition < sequence.Count &&
+                    !sequence[tilePosition - 1].IsGlueAfter &&
+                    !sequence[tilePosition].IsGlueBefore)
+                {
+                    charPosition++;
+                }
+            }
+
+            if (length == 0)
+            {
+                if (tilePosition < sequence.Count)
+                {
+                    if (charPosition < start)
+                    {
+                        var tile = sequence[tilePosition];
+                        tilePosition++;
+
+                        var prefix = new TileData(tile.Content.Substring(0, start - charPosition),
+                            isGlueBefore: tile.IsGlueBefore,
+                            isGlueAfter: true);
+                        list.Add(prefix);
+
+                        var caret = new TileData("^");
+                        list.Add(caret);
+
+                        var suffix = new TileData(tile.Content.Substring(start - charPosition),
+                            isGlueAfter: tile.IsGlueAfter, isGlueBefore: true);
+                        list.Add(suffix);
+                    }
+                    else
+                    {
+                        var caret = new TileData("^",
+                            isGlueBefore: start < charPosition,
+                            isGlueAfter: start == charPosition);
+                        list.Add(caret);
+                    }
+                }
+                else
+                {
+                    var caret = new TileData("^", isGlueBefore: true);
+                    list.Add(caret);
+                }
+
+            }
+            else
+            {
+
+            }
+
+            while (tilePosition < sequence.Count)
+            {
+                list.Add(sequence[tilePosition]);
+                tilePosition++;
+            }
+
+            CursedContainer.Items.Clear();
+            foreach (var child in list)
+            {
+                CursedContainer.Items.Add(child);
+            }
+
         }
     }
 }
