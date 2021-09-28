82bfa5e53bb18d90dd89cf8c790125043fc9e50e
Pete Ansell <petea@microsoft.com>
2021-09-28 16:29:07 -0700

    Improved code coverage.

---
+++ Microsoft.Research.SpeechWriter/Microsoft.Research.SpeechWriter.Core.Data.Test/TileTypeHelperTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace Microsoft.Research.SpeechWriter.Core.Data.Test
+{
+    public class TileTypeHelperTest
+    {
+        [Test]
+        public void FromElementNameTest()
+        {
+            Assert.AreEqual(TileType.Normal, TileTypeHelper.FromElementName("T"));
+            Assert.AreEqual(TileType.Prefix, TileTypeHelper.FromElementName("B"));
+            Assert.AreEqual(TileType.Suffix, TileTypeHelper.FromElementName("A"));
+            Assert.AreEqual(TileType.Infix, TileTypeHelper.FromElementName("J"));
+            Assert.AreEqual(TileType.Command, TileTypeHelper.FromElementName("C"));
+            Assert.Throws(typeof(InvalidDataException), () => TileTypeHelper.FromElementName("!"));
+        }
+
+        [Test]
+        public void ToElementNameTest()
+        {
+            Assert.AreEqual("T", TileTypeHelper.ToElementName(TileType.Normal));
+            Assert.AreEqual("B", TileTypeHelper.ToElementName(TileType.Prefix));
+            Assert.AreEqual("A", TileTypeHelper.ToElementName(TileType.Suffix));
+            Assert.AreEqual("J", TileTypeHelper.ToElementName(TileType.Infix));
+            Assert.AreEqual("C", TileTypeHelper.ToElementName(TileType.Command));
+        }
+    }
+}
---
+++ Microsoft.Research.SpeechWriter/Microsoft.Research.SpeechWriter.Core.Test/ApplicationModelTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Microsoft.Research.SpeechWriter.Core.Test
+{
+    public class ApplicationModelTest
+    {
+        [Test]
+        public void ShowTestCardTest()
+        {
+            var model = new ApplicationModel();
+            model.ShowTestCard();
+            model.ShowTestCard();
+        }
+    }
+}
